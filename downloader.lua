fa3dbae8fc3ff132478c9510c43902c1088aa1fe
John Neijzen <john.neijzen@live.com>
2017-01-21 01:19:16 +0800

    Update downloader.lua

--- downloader.lua
+++ downloader.lua
@@ -3,7 +3,7 @@ local function downloadExcavation()
 	local temp
 	local file
 
-	download = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Excavation2017.lua")
+	download = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Turtle%20Programs/Excavation2017.lua")
 	temp = download.readAll()
 	download.close()
 
@@ -13,7 +13,17 @@ local function downloadExcavation()
 end
 
 local function downloadTunnel()
+	local download
+	local temp
+	local file
 
+	download = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Turtle%20Programs/Tunnel2017.lua")
+	temp = download.readAll()
+	download.close()
+
+	file = fs.open("john-ComputerCraft-Program/Tunnel2017","w")
+	file.write(handle)
+	file.close()
 end
 
 local function update()
