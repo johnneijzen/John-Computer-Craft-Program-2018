427196340b7d074cf0681f590d1953ef7c2871a5
John Neijzen <john.neijzen@live.com>
2017-01-26 21:16:26 +0800

    Update downloader.lua

--- downloader.lua
+++ downloader.lua
@@ -1,51 +1,48 @@
+local download1 = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Turtle%20Programs/Excavation2017.lua")
+local download2 = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Turtle%20Programs/Tunnel2017.lua")
+local download3 = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/JohnPrograms.lua")
+ 
 local function downloadExcavation()
-	local download
-	local temp
-	local file
-
-	download = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Excavation2017.lua")
-	temp = download.readAll()
-	download.close()
-
-	file = fs.open("john-ComputerCraft-Program/Excavation2017","w")
-	file.write(handle)
-	file.close()
+    local file = 0
+    local temp = 0
+ 
+    temp = download1.readAll()
+    download1.close()
+ 
+    file = fs.open("john-ComputerCraft-Program/Excavation2017","w")
+    file.write(temp)
+    file.close()
 end
-
+ 
 local function downloadTunnel()
-	local download
-	local temp
-	local file
-
-	download = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Turtle%20Programs/Tunnel2017.lua")
-	temp = download.readAll()
-	download.close()
-
-	file = fs.open("john-ComputerCraft-Program/Tunnel2017","w")
-	file.write(handle)
-	file.close()
+    local file = 0
+    local temp = 0
+ 
+    temp = download2.readAll()
+    download2.close()
+ 
+    file = fs.open("john-ComputerCraft-Program/Tunnel2017","w")
+    file.write(temp)
+    file.close()
 end
-
+ 
 local function downloadJohnPrograms()
-	local download
-	local temp
-	local file
-
-	download = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/JohnPrograms.lua")
-	temp = download.readAll()
-	download.close()
-
-	file = fs.open("JohnPrograms","w")
-	file.write(handle)
-	file.close()
+    local file = 0
+    local temp = 0
+ 
+    temp = download3.readAll()
+    download3.close()
+ 
+    file = fs.open("JohnPrograms","w")
+    file.write(temp)
+    file.close()
 end
-
+ 
 local function update()
-	downloadExcavation()
-	downloadTunnel()
-	downloadJohnPrograms()
-	shell.run("JohnPrograms")
+    downloadExcavation()
+    downloadTunnel()
+    downloadJohnPrograms()
+    shell.run("JohnPrograms")
 end
-
+ 
 update()
-
