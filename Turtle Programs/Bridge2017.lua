1aabd1cfd41a29879548ff94fa55254d2fdb49e2
John Neijzen <john.neijzen@live.com>
2017-05-09 01:36:49 -0700

    Updates Testing Phase Tomorrow

---
+++ Turtle Programs/Bridge2017.lua
@@ -0,0 +1,169 @@
+--[[
+Version
+  0.01 9/5/2017
+Changelog
+  0.01 - First Draft
+--]]
+
+-- Locals Variables
+local noFuelNeeded = 0 -- Check if turtle is using no fuel config
+local itemFuel = turtle.getItemCount(1) -- Fuel Slot 1
+local itemFuel1 = turtle.getItemCount(2) -- Fuel Slot 2
+local distance = 0 -- Distance will dig
+local distanceCount = 0 -- Count the distance
+local missingFuel = 0 -- If there is missing fuel this will be 1
+local bridgeSize = 0 -- Brige Wide
+local currentSlot = 3 -- For checking on cobble
+local way = 0
+
+-- Checking On Items
+local function checking()
+    if noFuelNeeded == 0 then
+        if itemFuel == 0 then
+            missingFuel = 1
+        else
+            missingFuel = 0
+        end
+    end
+    if missingFuel == 1 then
+        print("Missing Fuel in Slot 1")
+    end
+end
+
+local function recheck()
+    itemFuel = turtle.getItemCount(1)
+    itemFuel1 = turtle.getItemCount(2)
+end
+
+local function reFuel()
+    if noFuelNeeded == 0 then
+        repeat
+            if turtle.getFuelLevel() < 120 then
+                if itemFuel > 1 then
+                    turtle.select(1)
+                    turtle.refuel(1)
+                    itemFuel = itemFuel - 1
+                    turtle.select(3)
+                elseif itemFuel1 > 1 then
+                    turtle.select(2)
+                    turtle.refuel(1)
+                    itemFuel1 = itemFuel1 - 1
+                    turtle.select(3)
+                else
+                    print("out of fuel")
+                    os.shutdown()
+                end
+            end
+        until turtle.getFuelLevel() >= 120
+    end
+end
+
+local function blockPlaceRight()
+    turtle.forward()
+    turtle.down()
+    turtle.placeDown()
+    turtle.up()
+    turtle.placeDown()
+    turtle.turnRight()
+    turtle.forward()
+    turtle.down()
+    turtle.placeDown()
+    turtle.forward()
+    turtle.placeDown()
+    turtle.forward()
+    turtle.placeDown()
+    turtle.forward()
+    turtle.placeDown()
+    if bridgeSize == 5 then
+        turtle.forward()
+        turtle.placeDown()
+    end
+    if bridgeSize == 5 or bridgeSize == 4 then
+        turtle.forward()
+        turtle.placeDown()
+    end
+    turtle.up()
+    turtle.placeDown()
+    turtle.turnLeft()
+end
+
+local function blockPlaceLeft()
+    turtle.forward()
+    turtle.down()
+    turtle.placeDown()
+    turtle.up()
+    turtle.placeDown()
+    turtle.turnLeft()
+    turtle.forward()
+    turtle.down()
+    turtle.placeDown()
+    turtle.forward()
+    turtle.placeDown()
+    turtle.forward()
+    turtle.placeDown()
+    turtle.forward()
+    turtle.placeDown()
+    if bridgeSize == 5 then
+        turtle.forward()
+        turtle.placeDown()
+    end
+    if bridgeSize == 5 or bridgeSize == 4 then
+        turtle.forward()
+        turtle.placeDown()
+    end
+    turtle.up()
+    turtle.placeDown()
+    turtle.turnRight()
+end
+
+function main()
+    reFuel()
+    turtle.select(3) -- this be cobble slot select
+    repeat
+        reFuel()
+        repeat
+            if turtle.getItemCount(currentSlot) <= 8 then -- this code will switch turtle slot when cobble is less than 7
+                currentSlot = currentSlot + 1
+            elseif turtle.getItemCount(currentSlot) >= 8 then
+                currentSlot = currentSlot
+            else
+                os.shutdown()
+            end
+        until turtle.getItemCount(currentSlot) >= 8
+        turtle.select(currentSlot)
+        if way == 0 then
+            blockPlaceRight()
+            way = 1
+        else
+            blockPlaceLeft()
+            way = 0
+        end
+        distanceCount = distanceCount + 1
+    until distance == distanceCount
+end
+
+function start()
+    print("Welcome To John Bridge Program")
+    print("This Program Make Brige Size From 3 Wide to 5 Wide")
+    print("Please Enter Brige Size")
+    bridgeSize = tonumber(read())
+    print("Please Input Your Fuel In Slot 1 and Slot 2(Optional)")
+    print("Please Input How Far Brige Will Be")
+    distance = tonumber(read())
+    print("Turtle Will Make " .. distance .. " Long Brige")
+    if turtle.getFuelLevel() == "unlimited" then -- just check if config of fuel is to unlimited
+        noFuelNeeded = 1
+    end
+    checking()
+    if missingFuel == 1 then
+        repeat
+            print("Please Put In Items That Are Missing")
+            sleep(10)
+            recheck()
+            checking()
+        until missingFuel == 0
+    end
+    main()
+end
+
+start()
--- downloader.lua
+++ downloader.lua
@@ -1,69 +1,71 @@
 --[[ 
     Version 
-        0.02
+        0.04
     Changelogs
         0.01 Added Support for Excavation2017.lua
              Added Support for Tunnel2017.lua
-        0.02 Added Support for StripMining.lua
+        0.02 Added Support for StripMining2017.lua
+        0.03 Added Pre Support for Bridge2017.lua
+        0.04 Compacting Code
 ]]--
 
 local download1 = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Turtle%20Programs/Excavation2017.lua")
 local download2 = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Turtle%20Programs/Tunnel2017.lua")
 local download3 = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/Turtle%20Programs/StripMining2017.lua")
-local download4 = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/JohnPrograms.lua")
- 
+local download4 = http.get("")
+local download5 = http.get("https://raw.githubusercontent.com/johnneijzen/John-Computer-Craft-Program-2017/master/JohnPrograms.lua")
+
 local function downloadExcavation()
-    local file = 0
-    local temp = 0
- 
-    temp = download1.readAll()
+    local temp = download1.readAll()
     download1.close()
  
     fs.delete("john-ComputerCraft-Program/Excavation2017") -- To make sure it updates
- 
-    file = fs.open("john-ComputerCraft-Program/Excavation2017","w")
+
+    local file = fs.open("john-ComputerCraft-Program/Excavation2017","w")
     file.write(temp)
     file.close()
 end
  
 local function downloadTunnel()
-    local file = 0
-    local temp = 0
- 
-    temp = download2.readAll()
+    local temp = download2.readAll()
     download2.close()
  
     fs.delete("john-ComputerCraft-Program/Tunnel2017") -- To make sure it updates
- 
-    file = fs.open("john-ComputerCraft-Program/Tunnel2017","w")
+
+    local file = fs.open("john-ComputerCraft-Program/Tunnel2017","w")
     file.write(temp)
     file.close()
 end
 
 local function downloadStripMining()
-    local file = 0
-    local temp = 0
- 
-    temp = download3.readAll()
+    local temp = download3.readAll()
     download3.close()
  
     fs.delete("john-ComputerCraft-Program/StripMining2017") -- To make sure it updates
- 
-    file = fs.open("john-ComputerCraft-Program/StripMining2017","w")
+
+    local file = fs.open("john-ComputerCraft-Program/StripMining2017","w")
+    file.write(temp)
+    file.close()
+end
+
+local function downloadBrige()
+    local temp = download4.readAll()
+    download4.close()
+
+    fs.delete("john-ComputerCraft-Program/Bridge2017") -- To make sure it updates
+
+    local file = fs.open("john-ComputerCraft-Program/Bridge2017","w")
     file.write(temp)
     file.close()
 end
  
 local function downloadJohnPrograms()
-    local file = 0
-    local temp = 0
- 
-    temp = download4.readAll()
-    download4.close()
+    local temp = download5.readAll()
+    download5.close()
 
     fs.delete("JohnPrograms") -- To make sure it updates
- 
-    file = fs.open("JohnPrograms","w")
+
+    local file = fs.open("JohnPrograms","w")
     file.write(temp)
     file.close()
 end
