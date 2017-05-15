befdc6c20ab2d0843150c568b8abbe2aa1510e14
John Neijzen <John.neijzen@live.com>
2017-05-16 00:44:50 +0800

    Starting Work On Multi Build Program

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ It Be Fully Rewrite of my old program in 2015 and 2016
 10% Strip Mining Program 2017
 
 10% Multi Build Program 2017
+
+Ideas To Compact code
+In Place using RSorLS maybe just use wideCount % 2 == 0 if is even
+run LS else run RS this will save one variable but i dont now if is better since
+wideCount can be very big number like 1000 then i think RSorLS is better.
--- Turtle Programs/MultiBuild2017.lua
+++ Turtle Programs/MultiBuild2017.lua
@@ -19,6 +19,8 @@ local width = 0
 local widthCount = 0
 local length = 0
 local lengthCount = 0
+local height
+local heightCount
 -- Program Variables
 local itemFuel = 0 -- Fuel Slot 1
 local itemFuel1 = 0 -- Fuel Slot 2
@@ -28,6 +30,9 @@ local errorItems = 0
 local n = 1
 local optionSize = 5
 local selectedOption
+-- Misc
+local AllowTurtleDig = 1
+local RsOrLs
 
 local options = {
     "Fill",
@@ -91,8 +96,54 @@ local function selectBuildBlocks()
     until turtle.getItemCount(buildBlocksSlot) > 0
 end
 
-local function fill()
+-- Build/Place Block
+local function placeDown()
+    if AllowTurtleDig == 1 then
+        if turtle.detectDown() then
+            turtle.digDown()
+        end
+    end
+    selectBuildBlocks()
+    turtle.placeDown()
+end
 
+-- modifyed forward this can destory block in way if AllowTurtleDig is 1
+local function forward()
+    if AllowTurtleDig == 1 then
+        if turtle.detect() then
+            turtle.dig()
+        end
+    end
+    turtle.forward()
+end
+
+local function fill()
+    repeat
+        repeat
+            refuel()
+            placeDown()
+            forward()
+            lengthCount = lengthCount + 1
+        until length == lengthCount - 1
+        widthCount = widthCount + 1
+        if width ~= widthCount then
+            refuel()
+            lengthCount = 0
+            if RsOrLs == 0 then
+                turtle.turnRight()
+                turtle.forward()
+                turtle.turnRight()
+                turtle.forward()
+                RsOrLs = 1
+            else
+                turtle.turnLeft()
+                turtle.forward()
+                turtle.turnLeft()
+                turtle.forward()
+                RsOrLs = 0
+            end
+        end
+    until width == widthCount and length == lengthCount
 end
 
 local function cube()
@@ -138,6 +189,9 @@ end
 
 local function start()
     print("Welcome to Multi Build Program")
+    print("Allow Turtle To Replace Blocks and Destory Blocks: 1(True) or 0(False)")
+    print("Recommend true but false is faster since it wont replace blocks or example when half it is already build")
+    AllowTurtleDig = tonumber(read())
     print("Running Options List: ")
     sleep(1)
     selectedOption = display()
@@ -148,12 +202,31 @@ local function start()
         width = tonumber(read())
         fill()
     elseif selectedOption == 2 then
+        print("Enter The Lenght: ")
+        length = tonumber(read())
+        print("Enter The Width: ")
+        width = tonumber(read())
+        print("Enter The Height ")
+        height = tonumber(read())
         cube()
     elseif selectedOption == 3 then
+
         circle()
     elseif selectedOption == 4 then
+        print("Enter The Lenght: ")
+        length = tonumber(read())
+        print("Enter The Width: ")
+        width = tonumber(read())
+        print("Enter The Height ")
+        height = tonumber(read())
         squareInSquareUpwards()
     elseif selectedOption == 5 then
+        print("Enter The Lenght: ")
+        length = tonumber(read())
+        print("Enter The Width: ")
+        width = tonumber(read())
+        print("Enter The Height ")
+        height = tonumber(read())
         squareInSquareDownwards()
     end
 
