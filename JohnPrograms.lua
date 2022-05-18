1a35e3d24c4225f57debb0916e4f1e26a20a7541
John Neijzen <john.neijzen@live.com>
2022-05-18 17:13:46 +0800

    Fixed Options with CC:tweaked

--- JohnPrograms.lua
+++ JohnPrograms.lua
@@ -15,8 +15,8 @@ local options = {
     "3x3 Tunnel Program 2017",
     "Strip Mining Program 2017 [WIP]",
     "Bridge Program 2017",
-    "Bridge Program with No Side Walls 2017",
     "Next Page",
+    "Bridge Program with No Side Walls 2017",
     "Multi Build Program 2017[WIP]",
     "Back Page"
 }
@@ -34,7 +34,7 @@ local function runOptions()
         shell.run("john-ComputerCraft-Program/StripMining2017")
     elseif n == 4 then
         shell.run("john-ComputerCraft-Program/Bridge2017")
-    elseif n == 5 then
+    elseif n == 6 then
         shell.run("john-ComputerCraft-Program/BridgeNoWalls2017")
     elseif n == 7 then
         shell.run("john-ComputerCraft-Program/MultiBuild2017")
@@ -45,31 +45,31 @@ local function gui()
     while true do
         term.clear()
         term.setCursorPos(1, 1)
-        for i = 1, 6 do
-            if i + ((currentPage - 1) * 6) <= optionSize then
-                if n == i + ((currentPage - 1) * 6) then
-                    print("---> " .. options[i + ((currentPage - 1) * 6)])
+        for i = 1, 5 do
+            if i + ((currentPage - 1) * 5) <= optionSize then
+                if n == i + ((currentPage - 1) * 5) then
+                    print("---> " .. options[i + ((currentPage - 1) * 5)])
                     print("")
                 else
-                    print("     " .. options[i + ((currentPage - 1) * 6)])
+                    print("     " .. options[i + ((currentPage - 1) * 5)])
                     print("")
                 end
             end
         end
         local event, key = os.pullEvent("key")
-        if key == keys.up and n > 1 + ((currentPage - 1) * 6) then
+        if key == keys.up and n > 1 + ((currentPage - 1) * 5) then
             n = n - 1
-        elseif key == keys.down and n < 6 + ((currentPage - 1) * 6) then
+        elseif key == keys.down and n < 5 + ((currentPage - 1) * 5) then
             if (n < optionSize) then
                 n = n + 1
             end
         elseif key == keys.enter then
-            if n == 6 then
+            if n == 5 then
                 currentPage = 2
-                n = 7
+                n = 6
             elseif n == 8 then
                 currentPage = 1
-                n = 6
+                n = 5
             else
                 break
             end
