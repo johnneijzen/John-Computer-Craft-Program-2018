ff662005721ff6ed898915a53e5d9016d602c521
John Neijzen <john.neijzen@live.com>
2024-04-11 01:32:55 +0800

    Update Bridge2017.lua

--- Turtle Programs/Bridge2017.lua
+++ Turtle Programs/Bridge2017.lua
@@ -1,58 +1,65 @@
---[[
+--[[ 
     Version
-    0.04 5/24/2021
+    0.06 11/04/2024
     Changelog
     0.01 - First Draft
     0.02 - Small Changes
-    0.03 - English is understandable
-    0.04 5/24/2021 - Code Formatting
+    0.03 - Improved readability
+    0.04 - Code formatting
+    0.05 - 11/04/2024 - Refactor code to allow switch slot for cobble without having to check for 8 or greater items
+    0.06 - code refactoring
 ]]
 
--- Locals Variables
+-- Local Variables
 local noFuelNeeded = 0 -- Check if turtle is using no fuel config
-local itemFuel = turtle.getItemCount(1)-- Fuel Slot 1
-local itemFuel1 = turtle.getItemCount(2)-- Fuel Slot 2
-local distance = 0 -- Distance will dig
+local itemFuel = 0 -- Fuel Slot 1
+local itemFuel2 = 0 -- Fuel Slot 2
+local distance = 0 -- Distance to dig
 local distanceCount = 0 -- Count the distance
 local errorItems = 0
-local bridgeSize = 0 -- Brige Wide
-local currentSlot = 3 -- For checking on cobble
+local bridgeSize = 0 -- Bridge Width
+local currentSlot = 3 -- For checking cobble
 local way = 0
 
-local function check()
+-- Function to switch to next slot if cobble slot is empty
+local function switchSlot()
+    currentSlot = currentSlot + 1
+    turtle.select(currentSlot)
+end
+
+-- Function to check for fuel and print error message if fuel is missing
+local function checkFuel()
     if noFuelNeeded == 0 then
-        if itemFuel == 0 then
+        if itemFuel == 0 and itemFuel2 == 0 then
+            print("Missing Fuel in Slot 1 or Slot 2")
             errorItems = 1
         else
             errorItems = 0
         end
     end
-    if errorItems == 1 then
-        print("Missing Fuel in Slot 1")
-    end
 end
 
+-- Function to update itemFuel and itemFuel2 counts
 local function itemCount()
     itemFuel = turtle.getItemCount(1)
-    itemFuel = turtle.getItemCount(2)
+    itemFuel2 = turtle.getItemCount(2)
 end
 
+-- Function to refuel turtle
 local function refuel()
     if noFuelNeeded == 0 then
         repeat
             if turtle.getFuelLevel() < 120 then
-                if itemFuel > 1 then
+                if itemFuel > 0 then
                     turtle.select(1)
                     turtle.refuel(1)
                     itemFuel = itemFuel - 1
-                    turtle.select(3)
-                elseif itemFuel1 > 1 then
+                elseif itemFuel2 > 0 then
                     turtle.select(2)
                     turtle.refuel(1)
-                    itemFuel1 = itemFuel1 - 1
-                    turtle.select(3)
+                    itemFuel2 = itemFuel2 - 1
                 else
-                    print("out of fuel")
+                    print("Out of fuel")
                     os.shutdown()
                 end
             end
@@ -60,108 +67,72 @@ local function refuel()
     end
 end
 
-local function blockPlaceRight()
+-- Function to place blocks on the right side
+local function placeBlocksRight()
     turtle.forward()
     turtle.down()
-    turtle.placeDown()
-    turtle.up()
-    turtle.placeDown()
-    turtle.turnRight()
-    turtle.forward()
-    turtle.down()
-    turtle.placeDown()
-    turtle.forward()
-    turtle.placeDown()
-    turtle.forward()
-    turtle.placeDown()
-    turtle.forward()
-    turtle.placeDown()
-    if bridgeSize == 5 then
-        turtle.forward()
+    switchSlot()
+    for _ = 1, bridgeSize do
         turtle.placeDown()
-    end
-    if bridgeSize == 5 or bridgeSize == 4 then
         turtle.forward()
-        turtle.placeDown()
+        switchSlot()
     end
     turtle.up()
-    turtle.placeDown()
-    turtle.turnLeft()
-end
-
-local function blockPlaceLeft()
-    turtle.forward()
-    turtle.down()
-    turtle.placeDown()
-    turtle.up()
-    turtle.placeDown()
-    turtle.turnLeft()
-    turtle.forward()
-    turtle.down()
-    turtle.placeDown()
-    turtle.forward()
-    turtle.placeDown()
-    turtle.forward()
-    turtle.placeDown()
-    turtle.forward()
-    turtle.placeDown()
-    if bridgeSize == 5 then
-        turtle.forward()
-        turtle.placeDown()
-    end
-    if bridgeSize == 5 or bridgeSize == 4 then
-        turtle.forward()
-        turtle.placeDown()
-    end
-    turtle.up()
-    turtle.placeDown()
     turtle.turnRight()
 end
 
-function main()
+-- Function to place blocks on the left side
+local function placeBlocksLeft()
+    turtle.forward()
+    turtle.down()
+    switchSlot()
+    turtle.turnLeft()
+    for _ = 1, bridgeSize do
+        turtle.placeDown()
+        turtle.forward()
+        switchSlot()
+    end
+    turtle.up()
+    turtle.turnRight()
+end
+
+-- Main function to construct the bridge
+local function constructBridge()
     refuel()
-    turtle.select(3)-- this is cobble slot select
+    turtle.select(3) -- Select cobble slot
     repeat
         refuel()
-        repeat
-            if turtle.getItemCount(currentSlot) <= 8 then -- this code will switch turtle slot when cobble is less than 7
-                currentSlot = currentSlot + 1
-            elseif turtle.getItemCount(currentSlot) >= 8 then
-                currentSlot = currentSlot
-            else
-                os.shutdown()
-            end
-        until turtle.getItemCount(currentSlot) >= 8
         turtle.select(currentSlot)
         if way == 0 then
-            blockPlaceRight()
+            placeBlocksRight()
             way = 1
         else
-            blockPlaceLeft()
+            placeBlocksLeft()
             way = 0
         end
         distanceCount = distanceCount + 1
     until distance == distanceCount
 end
 
-function start()
+-- Function to start the program
+local function start()
     print("Welcome To John's Bridge Program")
-    print("This Program Will Make A Brige From 3 Wide to 5 Wide")
-    print("Please Enter Brige Size")
+    print("This Program Will Make A Bridge From 3 Wide to 5 Wide")
+    print("Please Enter Bridge Size (3 to 5):")
     bridgeSize = tonumber(read())
-    print("Please Input Your Fuel In Slot 1 and Slot 2(Optional) Slot 3-15 Building Blocks")
-    print("Please Input How Long Brige Will Be")
+    print("Please Input Your Fuel In Slot 1 and Slot 2 (Optional). Slot 3-15 Will Be Used For Building Blocks.")
+    print("Please Input The Length Of The Bridge:")
     distance = tonumber(read())
-    print("Turtle Will Make " .. distance .. " Long Brige")
-    if turtle.getFuelLevel() == "unlimited" then -- just check if config of fuel is to unlimited
+    print("Turtle Will Make " .. distance .. " Long Bridge")
+    if turtle.getFuelLevel() == "unlimited" then
         noFuelNeeded = 1
     end
     repeat
         itemCount()
-        check()
+        checkFuel()
         sleep(5)
     until errorItems == 0
-    main()
+    constructBridge()
 end
 
 start()
